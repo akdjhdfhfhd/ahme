caec49ab33ec2f4185d99cf1b1834c17b6936f45
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-02-27 19:02:58 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -1,50 +1,192 @@
-from typing import Union
+# MIT License
 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+#
+
+# Copyright (c) 2023 AnonymousX1025
+
+#
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+
+# of this software and associated documentation files (the "Software"), to deal
+
+# in the Software without restriction, including without limitation the rights
+
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+
+# copies of the Software, and to permit persons to whom the Software is
+
+# furnished to do so, subject to the following conditions:
+
+#
+
+# The above copyright notice and this permission notice shall be included in all
+
+# copies or substantial portions of the Software.
+
+#
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+
+# SOFTWARE.
+
+from pyrogram import filters
+
+from pyrogram.enums import ChatType, ParseMode
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+from youtubesearchpython.__future__ import VideosSearch
 
 import config
 
+from FallenMusic import BOT_MENTION, BOT_NAME, app
 
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="أضف البوت لمجموعتڪ✅",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="مساعده",
-                callback_data="settings_back_helper",
-            ),
-            InlineKeyboardButton(
-                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
-            ),
-        ],
-     ]
-    return buttons
+from FallenMusic.Helpers import gp_buttons, pm_buttons
 
+from FallenMusic.Helpers.dossier import *
 
-def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="أضف البوت لمجموعتڪ✅",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+@app.on_message(filters.command(["start"]) & ~filters.forwarded)
+
+@app.on_edited_message(filters.command(["start"]) & ~filters.forwarded)
+
+async def fallen_st(_, message: Message):
+
+    if message.chat.type == ChatType.PRIVATE:
+
+        if len(message.text.split()) > 1:
+
+            cmd = message.text.split(None, 1)[1]
+
+            if cmd[0:3] == "inf":
+
+                m = await message.reply_text("⚡")
+
+                query = (str(cmd)).replace("info_", "", 1)
+
+                query = f"https://www.youtube.com/watch?v={query}"
+
+                results = VideosSearch(query, limit=1)
+
+                for result in (await results.next())["result"]:
+
+                    title = result["title"]
+
+                    duration = result["duration"]
+
+                    views = result["viewCount"]["short"]
+
+                    thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+
+                    channellink = result["channel"]["link"]
+
+                    channel = result["channel"]["name"]
+
+                    link = result["link"]
+
+                    published = result["publishedTime"]
+
+                searched_text = f"""
+
+⎊ **ᴛʀᴀᴄᴋ ɪɴғᴏʀɴᴀᴛɪᴏɴ** 
+
+📌 **ᴛɪᴛʟᴇ :** {title}
+
+⏳ **ᴅᴜʀᴀᴛɪᴏɴ :** {duration} ᴍɪɴᴜᴛᴇs
+
+👀 **ᴠɪᴇᴡs :** `{views}`
+
+⏰ **ᴩᴜʙʟɪsʜᴇᴅ ᴏɴ :** {published}
+
+🔗 **ʟɪɴᴋ :** [ᴡᴀᴛᴄʜ ᴏɴ ʏᴏᴜᴛᴜʙᴇ]({link})
+
+🎥 **ᴄʜᴀɴɴᴇʟ :** [{channel}]({channellink})
+
+💖 sᴇᴀʀᴄʜ ᴩᴏᴡᴇʀᴇᴅ ʙʏ {BOT_NAME}"""
+
+                key = InlineKeyboardMarkup(
+
+                    [
+
+                        [
+
+                            InlineKeyboardButton(text="ʏᴏᴜᴛᴜʙᴇ", url=link),
+
+                            InlineKeyboardButton(
+
+                                text="sᴜᴩᴩᴏʀᴛ", url=config.SUPPORT_CHAT
+
+                            ),
+
+                        ],
+
+                    ]
+
+                )
+
+                await m.delete()
+
+                return await app.send_photo(
+
+                    message.chat.id,
+
+                    photo=thumbnail,
+
+                    caption=searched_text,
+
+                    parse_mode=ParseMode.MARKDOWN,
+
+                    reply_markup=key,
+
+                )
+
+        else:
+
+            await message.reply_photo(
+
+                photo=config.START_IMG,
+
+                caption=PM_START_TEXT.format(
+
+                    message.from_user.first_name,
+
+                    BOT_MENTION,
+
+                ),
+
+                reply_markup=InlineKeyboardMarkup(pm_buttons),
+
             )
-        ],
-        [
-            InlineKeyboardButton(
-                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="❣ قناة المطوࢪ⚙️ ❣", url=config.SUPPORT_GROUP
+
+    else:
+
+        await message.reply_photo(
+
+            photo=config.START_IMG,
+
+            caption=START_TEXT.format(
+
+                message.from_user.first_name,
+
+                BOT_MENTION,
+
+                message.chat.title,
+
+                config.SUPPORT_CHAT,
+
             ),
-            InlineKeyboardButton(
-                text="🥀 مطوࢪ البوت👨‍💻 🥀", user_id=OWNER
-            )
-        ],
-      
+
+            reply_markup=InlineKeyboardMarkup(gp_buttons),
+
+        )
+ 
