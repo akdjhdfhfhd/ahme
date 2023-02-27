102cbf7e021ad7e6ef9e6476c73b070a977a0267
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-02-27 16:26:39 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.https://t.me/ah07v
+                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.UPSTREAM_REPO
             )
         ],
      ]
