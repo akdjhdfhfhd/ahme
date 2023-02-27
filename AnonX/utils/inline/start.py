858962da761c4d707dd83e62277954c95525953a
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-02-27 17:48:08 +0300

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,13 +9,13 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                text="أضف البوت لمجموعتڪ✅",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="ʜᴇʟᴩ",
+                text="مساعده",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
